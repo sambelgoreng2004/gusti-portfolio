bb27b13851ed8d329039d974d94c7ec5691b02b7
sambelgoreng2004 <gustialtapradana2004@gmail.com>
2026-01-27 22:52:46 +0800

    first deploy portfolio

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask import request
+app = Flask(__name__)
+
+projects = [
+    {
+        "title": "Web Portfolio Flask",
+        "desc": "Personal portfolio website built using Flask and Jinja template.",
+        "tech": "Python, Flask, HTML, CSS"
+    },
+    {
+        "title": "Login System",
+        "desc": "Authentication system with session management.",
+        "tech": "Flask, SQLite"
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", projects=projects)
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    print("NEW CLIENT:")
+    print(name, email, message)
+
+    return "Message Sent Successfully!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
